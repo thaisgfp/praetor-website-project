d7e4811b62e93e977de18869ee6cb4708368eb6e
Thaís Fernandes <fernandesthais674@gmail.com>
2026-04-27 12:53:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+function abrirModalAutomacao(){
+document.getElementById("overlayAutomacao").style.display = "flex"
+}
+
+function fecharModalAutomacao(){
+document.getElementById("overlayAutomacao").style.display = "none"
+}
+
+
+// MARKETING
+function abrirModalMarketing(){
+document.getElementById("overlayMarketing").style.display = "flex"
+}
+
+function fecharModalMarketing(){
+document.getElementById("overlayMarketing").style.display = "none"
+}
+
+
+// WEBSITE
+function abrirModalWebsite(){
+document.getElementById("overlayWebsite").style.display = "flex"
+}
+
+function fecharModalWebsite(){
+document.getElementById("overlayWebsite").style.display = "none"
+}
